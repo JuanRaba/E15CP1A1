8976cf2b81ed365e368eb8407b5ae9c8510b57d2
juanraba <jrabanillog@gmail.com>
2018-09-24 19:59:22 -0300

    session destroy, new

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def logged?
+    session[:user_id].present? # present? returns boolean :/ ? true : false 
+  end
+  def current_user
+    User.find(session[:user_id]) 
+  end
 end
